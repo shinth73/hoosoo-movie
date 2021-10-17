88fddc6148c1512a85f2b4e16bc69818b10e998a
marsinn <shinth73@gmail.com>
2021-10-17 16:40:21 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import AppLoading from 'expo-app-loading';
+import { Image, useColorScheme } from 'react-native';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
+import { Asset } from 'expo-asset';
+import { NavigationContainer } from '@react-navigation/native';
+import { ThemeProvider } from 'styled-components/native';
+import Root from './navigation/Root';
+import { darkTheme, lightTheme } from './styled';
+
+const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+const loadImages = (images) =>
+    images.map((image) => {
+        if (typeof image === 'string') {
+            return Image.prefetch(image);
+        } else {
+            return Asset.loadAsync(image);
+        }
+    });
+
+export default function App() {
+    const [ready, setReady] = useState(false);
+    const onFinish = () => setReady(true);
+    const startLoading = async () => {
+        await Font.loadAsync(Ionicons.font);
+        const fonts = loadFonts([Ionicons.font]);
+        const images = loadImages([
+            // require("./my-face.jpeg"),
+            'https://reactnative.dev/img/oss_logo.png',
+        ]);
+        await Promise.all([...fonts, ...images]);
+    };
+    const isDark = useColorScheme() === 'dark';
+    if (!ready) {
+        return (
+            <AppLoading
+                startAsync={startLoading}
+                onFinish={onFinish}
+                onError={console.error}
+            />
+        );
+    }
+    return (
+        <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+            <NavigationContainer>
+                <Root />
+            </NavigationContainer>
+        </ThemeProvider>
+    );
+}
