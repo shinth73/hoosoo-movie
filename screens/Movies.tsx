33922a6fb8b0ac1c2447f4663bd9541075ecfefb
marsinn <shinth73@gmail.com>
2021-10-17 22:48:34 +0900

    i dont understand blur error in android

---
+++ screens/Movies.tsx
@@ -0,0 +1,78 @@
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, Dimensions, StyleSheet } from 'react-native';
+import Swiper from 'react-native-web-swiper';
+import styled from 'styled-components/native';
+import { BlurView } from 'expo-blur';
+import { makeImgPath } from '../utils';
+import useColorScheme from 'expo-status-bar/build/useColorScheme';
+
+const API_KEY = '8f84640eaec345addf5f99ad785ed454';
+
+const Container = styled.ScrollView``;
+
+const View = styled.View`
+    flex: 1;
+`;
+
+const Loader = styled.View`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+`;
+
+const BgImg = styled.Image``;
+
+const Title = styled.Text``;
+
+const { height: SCREEN_HEIGHT } = Dimensions.get('window');
+
+const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = () => {
+    const isDark = useColorScheme === 'dark';
+    const [loading, setLoading] = useState(true);
+    const [nowPlaying, setNowPlaying] = useState([]);
+    const getNowPlaying = async () => {
+        const { results } = await (
+            await fetch(
+                `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR`,
+            )
+        ).json();
+        setNowPlaying(results);
+        setLoading(false);
+    };
+    useEffect(() => {
+        getNowPlaying();
+    }, []);
+    return loading ? (
+        <Loader>
+            <ActivityIndicator />
+        </Loader>
+    ) : (
+        <Container>
+            <Swiper
+                loop
+                timeout={3.5}
+                controlsEnabled={false}
+                containerStyle={{ width: '100%', height: SCREEN_HEIGHT / 4 }}
+            >
+                {nowPlaying.map((movie) => (
+                    <View key={movie.id}>
+                        <BgImg
+                            style={StyleSheet.absoluteFill}
+                            source={{ uri: makeImgPath(movie.backdrop_path) }}
+                        />
+                        <BlurView
+                            tint={isDark ? 'dark' : 'light'}
+                            intensity={80}
+                            style={StyleSheet.absoluteFill}
+                        >
+                            <Title>{movie.original_title}</Title>
+                        </BlurView>
+                    </View>
+                ))}
+            </Swiper>
+        </Container>
+    );
+};
+
+export default Movies;
